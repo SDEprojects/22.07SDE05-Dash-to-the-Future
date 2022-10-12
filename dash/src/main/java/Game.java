d5905583a96456c106f6287ac160a4cf88befe1c
DTNevaeha <DTNevaeha@gmail.com>
2022-10-12 15:36:49 -0500

    adding to git ignore file AB#259

--- dash/src/main/java/Game.java
+++ dash/src/main/java/Game.java
@@ -8,12 +8,13 @@ public class Game {
             + " and attack zombies. The objective of the game is to either find a way back \n"
             + " to the past or at least find a safe haven.\n");
 
-  }
-
-}
-
-//if user types help then run this...
+    //if user types help then run this...
     System.out.println("\n These are all the options avaliable to the player. Not all options are"
         + "available to use at all times \n Try typing some of these commands below \n"
         + "go east \n   -This moves the player in the specified direction you may also"
         + " try north, south, or west \n talk npc \n   -This ");
+  }
+
+}
+
+
