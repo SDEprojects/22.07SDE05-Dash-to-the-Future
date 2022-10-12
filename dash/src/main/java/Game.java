834b2823fcb176b9643311816da6bfe6490b4373
DTNevaeha <DTNevaeha@gmail.com>
2022-10-12 14:07:29 -0500

    Objectives: AB#256

--- dash/src/main/java/Game.java
+++ dash/src/main/java/Game.java
@@ -1,3 +1,14 @@
+
 public class Game {
 
+  public static void main(String[] args) {
+    System.out.println(
+        "This is a text-based RPG game that features a playable character that wanders around"
+            + " in an apocalyptic future. The character can talk to NPC, pick up items,"
+            + " and attack zombies. The objective of the game is to either find a way back"
+            + " to the past or at least find a safe haven.");
+
+  }
+
 }
+
